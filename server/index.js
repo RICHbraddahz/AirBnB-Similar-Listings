675d2ce20edc56427190e624f0203e5b144f87ec
Richard Hai <hirichardhai@gmail.com>
2018-03-24 11:13:51 -0700

    updated yml test configuration and moved newrelic in index.js to the top

--- server/index.js
+++ server/index.js
@@ -1,27 +1,27 @@
 const nr = require('newrelic');
 const mongoose = require('mongoose');
 const express = require('express');
-// const morgan = require('morgan');
+const morgan = require('morgan');
 const path = require('path');
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
-// require('dotenv').config();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
 const listings = require('../db/listing').query;
 
-// const PORT = parseInt(process.env.PORT, 10);
-// const DB_NAME = process.env.DB_NAME;
-// const HOST = process.env.HOST;
-// const PROXY_PORT = parseInt(process.env.PROXY_PORT, 10);
+const PORT = parseInt(process.env.PORT, 10);
+const DB_NAME = process.env.DB_NAME;
+const HOST = process.env.HOST;
+const PROXY_PORT = parseInt(process.env.PROXY_PORT, 10);
 
 const app = express();
 
 const PUBLIC_DIR = path.join(__dirname, '../public');
 
 // app.use(morgan('dev'));
-// app.use(cors(`http://${HOST}:${PROXY_PORT}`))
+app.use(cors(`http://${HOST}:${PROXY_PORT}`))
 app.use('/listings/:id', express.static(PUBLIC_DIR));
-// app.use(express.static(PUBLIC_DIR));
-// app.use(bodyParser.json());
+app.use(express.static(PUBLIC_DIR));
+app.use(bodyParser.json());
 
 // app.get('/listings/:id/similar_listings', async ({ params: { id } }, res) => {
 //   try {
@@ -41,4 +41,4 @@ app.get('/listings/:id/similar_listings', (req, res) => {
 });
 
 mongoose.connect('mongodb://localhost/seabnb');
-// const server = app.listen(PORT, HOST, () => console.log(`listening on ${HOST}:${PORT}`));
+const server = app.listen(PORT, HOST, () => console.log(`listening on ${HOST}:${PORT}`));
