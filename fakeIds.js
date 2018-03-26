72029ad78c9a408da8fb5231e931598d2eb9e015
Richard Hai <hirichardhai@gmail.com>
2018-03-26 15:25:26 -0700

    removed faker from fakeIds generator

--- fakeIds.js
+++ fakeIds.js
@@ -1,7 +1,5 @@
 'use strict';
 // Make sure to "npm install faker" first.
-const Faker = require('faker');
-
 const generateRandomId = (userContext, events, done) => {
   const id = Math.floor(Math.random() * 9999997);
   userContext.vars.id = id;
