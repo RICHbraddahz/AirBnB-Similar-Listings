113bacced9a5ce4d07c5ab354cfe3b47c6e2b593
Richard Hai <hirichardhai@gmail.com>
2018-12-09 23:51:37 -0800

    fixed syntax

--- fakeIds.js
+++ fakeIds.js
@@ -1,5 +1,7 @@
 'use strict';
+
 // Make sure to "npm install faker" first.
+
 const generateRandomId = (userContext, events, done) => {
   const id = Math.floor(Math.random() * 9999997);
   userContext.vars.id = id;
